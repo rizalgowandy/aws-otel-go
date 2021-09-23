804e134ff91c4cf939c2d8038a4ff106615502f8
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 20:09:18 +0000

    add Soak Test Results - sample-app-golang_alpine-manual (custombenchmark) benchmark result for f6a44fb21705cdb00211329a44d82b893a384931

---
+++ soak-tests/per-commit-overall-results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632427758254,
+  "repoUrl": "https://github.com/aws-observability/aws-otel-go",
+  "entries": {
+    "Soak Test Results - sample-app-golang_alpine-manual": [
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f6a44fb21705cdb00211329a44d82b893a384931",
+          "message": "Edits for compatability with aws-otel-collector v13 (#102)",
+          "timestamp": "2021-09-22T22:58:32Z",
+          "url": "https://github.com/aws-observability/aws-otel-go/commit/f6a44fb21705cdb00211329a44d82b893a384931"
+        },
+        "date": 1632427757835,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 28.400059523809524,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 700.48046875,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
